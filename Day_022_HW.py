c559f8e98d62f44ae816a74a96c1e300e273e0b7
youjhen <36754928+youjhen@users.noreply.github.com>
2020-12-21 21:24:15 +0800

    Add files via upload

---
+++ Day_022_HW.py
@@ -0,0 +1,61 @@
+"""
+1.下列A~E五個程式區塊中，哪一塊是特徵工程?
+C
+2.對照程式區塊 B 與 C 的結果，請問那些欄位屬於"類別型欄位"? 
+(回答欄位英文名稱即可)
+Pclass        int64
+Sex          object
+SibSp         int64
+Parch         int64
+Embarked     object
+# 3.續上題，請問哪個欄位是"目標值"?
+Survived
+"""
+
+# 程式區塊 A：讀取訓練與測試資料
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+
+data_path = 'C:/Users/tuser.LAPTOP-QMJF8JVH/Downloads/'
+df_train = pd.read_csv(data_path + 'titanic_train.csv')
+df_test = pd.read_csv(data_path + 'titanic_test.csv')
+df_train.shape
+#(891, 12)
+
+# 程式區塊 B：在此先抽離出 PassengerId 與 Survived, 
+# 而先將 df_train, df_test 該有的資料合併成 df, 先作特徵工程
+train_Y = df_train['Survived']
+ids = df_test['PassengerId']
+df_train = df_train.drop(['PassengerId', 'Survived'] , axis=1)
+df_test = df_test.drop(['PassengerId'] , axis=1)
+df = pd.concat([df_train,df_test])
+df.head().dtypes
+
+# 程式區塊 C：特徵工程-簡化版 : 全部空值先補-1
+LEncoder = LabelEncoder()
+MMEncoder = MinMaxScaler()
+for c in df.columns:
+    df[c] = df[c].fillna(-1)
+    if df[c].dtype == 'object':
+        df[c] = LEncoder.fit_transform(list(df[c].values))
+    df[c] = MMEncoder.fit_transform(df[c].values.reshape(-1, 1))
+df.head().dtypes
+
+""" 程式區塊 D
+將前述轉換完畢資料 df , 重新切成 train_X, test_X, 因為不論何種特徵工程, 都需要對 train / test 做同樣處理
+常見並簡便的方式就是 - 先將 train / test 接起來, 做完後再拆開, 不然過程當中往往需要將特徵工程部分寫兩次, 麻煩且容易遺漏
+在較複雜的特徵工程中尤其如此, 若實務上如果碰到 train 與 test 需要分階段進行, 則通常會另外寫成函數處理
+"""
+train_num = train_Y.shape[0]
+train_X = df[:train_num]
+test_X = df[train_num:]
+
+from sklearn.linear_model import LogisticRegression
+estimator = LogisticRegression()
+estimator.fit(train_X, train_Y)
+pred = estimator.predict(test_X)
+
+# 程式區塊 E
+sub = pd.DataFrame({'PassengerId': ids, 'Survived': pred})
+sub.to_csv('titanic_baseline.csv', index=False) 
